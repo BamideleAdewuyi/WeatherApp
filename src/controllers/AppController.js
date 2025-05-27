31b7063eb6466bbe93a604d395461af7a0d9d1cf
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-27 23:16:47 +0100

    AppController.js: Made draft of getLocationData. Calls API. Need to make it async. Also need to URL encode search so we can search for things like London, Ontario

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -4,7 +4,14 @@ class AppController {
     }
 
     getLocationData(location) {
-        
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5VAXRJV6HJN8XMXDJR8W98TRW`;
+        fetch(url)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(response) {
+                console.log(response)
+            })
     }
 }
 
