9b91e8ba62a46b976a1634cbac15a070f85193ee
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-27 22:37:07 +0100

    Location.js: Created and filled in Location class. Should be able to create this with data from API request

---
+++ src/models/Location.js
@@ -0,0 +1,12 @@
+class Location {
+    constructor(location, realTemp, feelsLikeTemp, rain, sunrise, sunset) {
+        this.location = location;
+        this.realTemp = realTemp;
+        this.feelsLikeTemp = feelsLikeTemp;
+        this.rain = rain;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+    }
+}
+
+export default Location;
