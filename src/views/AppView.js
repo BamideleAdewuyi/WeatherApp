c63b448b77b44319ec20ab45bf83d2567dfd396c
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-03 23:49:58 +0100

    AppView.js: Added container to constructor

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -3,9 +3,10 @@ import AppController from "../controllers/AppController";
 class AppView {
     constructor() {
         this.appController = new AppController;
+        this.container = document.querySelector('.container')
     }
 
     createSearchBar() {
-        
+
     }
 }
