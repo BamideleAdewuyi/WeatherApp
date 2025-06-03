14b69c79b2ecdba3c8d562cb63ef902390829336
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-04 00:09:55 +0100

    AppView.js: Wrote createForm method. May not work

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -8,5 +8,14 @@ class AppView {
 
     createForm() {
         const form = document.createElement('form');
+        const searchBarLabel = document.createElement('label');
+        const searchBar = document.createElement('input');
+        searchBar.id = 'searchBar'
+        searchBar.setAttribute('type', 'text');
+        searchBar.setAttribute('placeholder', 'Enter location');
+        searchBarLabel.setAttribute('for', 'searchBar');
+        searchBarLabel.textContent = 'Search';
+        form.append(searchBarLabel, searchBar)
+        this.container.append(form)
     }
 }
