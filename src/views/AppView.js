2810588acd64736bcf3b7579beb16cd164cd29cf
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-04 15:47:00 +0100

    AppView.js: Added nextWeekData to constructor

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -3,7 +3,8 @@ import AppController from "../controllers/AppController";
 class AppView {
     constructor() {
         this.appController = new AppController;
-        this.container = document.querySelector('.container')
+        this.container = document.querySelector('.container');
+        this.nextWeekData = {}
     }
 
     clearContainer() {
