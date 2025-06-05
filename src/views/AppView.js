b24589c35084a42a00ee848e92e0a8d3df557ce3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-05 01:12:34 +0100

    AppView.js: Removed import. Cleared constructor apart from container

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -1,11 +1,6 @@
-import AppController from "../controllers/AppController";
-
 class AppView {
     constructor() {
-        this.appController = new AppController;
         this.container = document.querySelector('.container');
-        this.nextWeekData = {}
-        this.initialise()
     }
 
     clearContainer() {
