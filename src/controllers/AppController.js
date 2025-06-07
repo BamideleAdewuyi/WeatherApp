8438e782cdc15d09cec9ea77b43475c5dfd9d8e2
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-08 00:27:33 +0100

    AppController.js: handleSearchLocation is async. Search bar now sets new location

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -2,10 +2,11 @@ class AppController {
     constructor(model, view) {
         this.model = model;
         this.view = view;
+        this.view.bindSearchLocation(this.handleSearchLocation)
     }
 
-    handleSearchLocation(location) {
-        this.model
+    handleSearchLocation = async (userInput) => {
+        await this.model.getLocationData(userInput);
     }
 }
 
