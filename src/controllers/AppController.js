a9b8e5e4c1b2f667daa5a48471d1651a4103fe59
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-08 01:45:13 +0100

    AppController.js: Wrote handleSelectDay method. Not sure if it works

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -14,8 +14,11 @@ class AppController {
         this.view.displayForecastContent(nextWeekData[0])
     }
 
-    handleSelectDay = (day) => {
-        
+    handleSelectDay = (displayLinkIndex) => {
+        const nextWeekData = this.model.getNextWeekData();
+        this.view.displayLinks(nextWeekData);
+        this.view.displayDayTab(nextWeekData, displayLinkIndex)
+        this.view.displayForecastContent(nextWeekData[displayLinkIndex])
     }
 }
 
