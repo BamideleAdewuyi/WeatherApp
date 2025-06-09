24134648df4272ef8f7d83238ced6acb9049d5b6
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-09 01:39:50 +0100

    imageMap.js: Wrote and exported ImageMap const

---
+++ src/views/imageMap.js
@@ -0,0 +1,47 @@
+import clearDay from './assets/clear-day.png';
+import clearNight from './assets/clear-night.png';
+import cloudy from './assets/cloudy.png';
+import fog from './assets/fog.png';
+import hail from './assets/hail.png';
+import partlyCloudyDay from './assets/partly-cloudy-day.png';
+import partlyCloudyNight from './partly-cloudy-night.png';
+import rainSnowShowersDay from './assets/rain-snow-showers-day.png';
+import rainSnowShowersNight from './assets/rain-snow-showers-night.png';
+import rainSnow from './assets/rain-snow.png';
+import rain from './assets/rain.png`';
+import showersDay from './assets/showers-day.png';
+import showersNight from './assets/showers-night.png';
+import sleet from './assets/sleet.png';
+import snowShowersDay from './assets/snow-showers-day.png';
+import snowShowersNight from './assets/snow-showers-night.png';
+import snow from './assets/snow.png';
+import thunderRain from './assets/thunder-rain.png';
+import thunderShowersDay from './assets/thunder-showers-day.png';
+import thunderShowersNight from './assets/thunder-showers-night.png';
+import thunder from './assets/thunder.png';
+import wind from './assets/wind.png';
+
+export const ImageMap = {
+    'clear-day': clearDay,
+    'clear-night': clearNight,
+    'cloudy': cloudy,
+    'fog': fog,
+    'hail': hail,
+    'partly-cloudy-day': partlyCloudyDay,
+    'partly-cloudy-night': partlyCloudyNight,
+    'rain-snow-showers-day': rainSnowShowersDay,
+    'rain-snow-showers-night': rainSnowShowersNight,
+    'rain-snow': rainSnow,
+    'rain': rain,
+    'showers-day': showersDay,
+    'showers-night': showersNight,
+    'sleet': sleet,
+    'snow-showers-day': snowShowersDay,
+    'snow-showers-night': snowShowersNight,
+    'snow': snow,
+    'thunder-rain': thunderRain,
+    'thunder-showers-day': thunderShowersDay,
+    'thunder-showers-night': thunderShowersNight,
+    'thunder': thunder,
+    'wind': wind
+}
