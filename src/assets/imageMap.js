d0fcbeae201d232ef143261d24a9bff324b446b9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-09 01:49:23 +0100

    Moved imageMap.js to assets

---
+++ src/assets/imageMap.js
@@ -0,0 +1,47 @@
+import clearDay from './clear-day.png';
+import clearNight from './clear-night.png';
+import cloudy from './cloudy.png';
+import fog from './fog.png';
+import hail from './hail.png';
+import partlyCloudyDay from './partly-cloudy-day.png';
+import partlyCloudyNight from './partly-cloudy-night.png';
+import rainSnowShowersDay from './rain-snow-showers-day.png';
+import rainSnowShowersNight from './rain-snow-showers-night.png';
+import rainSnow from './rain-snow.png';
+import rain from './rain.png';
+import showersDay from './showers-day.png';
+import showersNight from './showers-night.png';
+import sleet from './sleet.png';
+import snowShowersDay from './snow-showers-day.png';
+import snowShowersNight from './snow-showers-night.png';
+import snow from './snow.png';
+import thunderRain from './thunder-rain.png';
+import thunderShowersDay from './thunder-showers-day.png';
+import thunderShowersNight from './thunder-showers-night.png';
+import thunder from './thunder.png';
+import wind from './wind.png';
+
+export const ImageMap = {
+    'clear-day': clearDay,
+    'clear-night': clearNight,
+    'cloudy': cloudy,
+    'fog': fog,
+    'hail': hail,
+    'partly-cloudy-day': partlyCloudyDay,
+    'partly-cloudy-night': partlyCloudyNight,
+    'rain-snow-showers-day': rainSnowShowersDay,
+    'rain-snow-showers-night': rainSnowShowersNight,
+    'rain-snow': rainSnow,
+    'rain': rain,
+    'showers-day': showersDay,
+    'showers-night': showersNight,
+    'sleet': sleet,
+    'snow-showers-day': snowShowersDay,
+    'snow-showers-night': snowShowersNight,
+    'snow': snow,
+    'thunder-rain': thunderRain,
+    'thunder-showers-day': thunderShowersDay,
+    'thunder-showers-night': thunderShowersNight,
+    'thunder': thunder,
+    'wind': wind
+}
