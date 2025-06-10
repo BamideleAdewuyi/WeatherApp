26d5fe531ec505c1f8b620a091185eebdf9a60d3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-10 22:32:52 +0100

    index.js: Created app const so app starts when file is opened

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import AppController from "./controllers/AppController";
 import AppModel from "./models/AppModel";
 import AppView from "./views/AppView";
 import "./style.css";
+
+const app = new AppController(new AppModel, new AppView);
